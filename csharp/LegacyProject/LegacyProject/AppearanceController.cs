9e2d4b8bf100c10ae42d30c3b6c868e0b744446e
Michael Ibarra <mibarra@aimconsulting.com>
2017-06-22 15:33:45 -0700

    added api controller

---
+++ csharp/LegacyProject/LegacyProject/AppearanceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Web.Http;
+using LegacyProject.BookSigning;
+
+namespace LegacyProject
+{
+    public class AppearanceController: ApiController
+    {
+        private readonly IAppearanceRepository _repository;
+
+        public AppearanceController(IAppearanceRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public IHttpActionResult UpdateAppearance(int id, [FromBody] Appearance appearance)
+        {
+            if (!User.IsInRole("Administrator"))
+            {
+                return Unauthorized();
+            }
+
+            try
+            {
+                _repository.UpdateAppearance(appearance);
+            }
+            catch (Exception exception)
+            {
+                return InternalServerError(exception);
+            }
+            return StatusCode(HttpStatusCode.Accepted);
+        }
+    }
+
+    public interface IAppearanceRepository
+    {
+        void UpdateAppearance(Appearance appearance);
+    }
+}
